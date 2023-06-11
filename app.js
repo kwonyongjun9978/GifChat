9e3ce7a4ff09af43c8208285c7351b235cc05831
Jun <rnsuz74@naver.com>
2023-06-11 21:57:57 +0900

    gif_Chat

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const connect = require('./schemas');
+const ColorHash = require('color-hash').default;
+dotenv.config();
+
+const webSocket = require('./socket');
+const indexRouter = require('./routes');
+const ColorHash = require('color-hash');
+
+const app = express();
+app.set('port', process.env.PORT || 8005);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+connect();
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/gif', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+const sessionMiddleware = session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+});
+app.use(sessionMiddleware);
+
+//user정보를 db에 저장하지 않기때문에 id를 세션을 통해서 생성
+app.use((req, res, next) => {
+    if(!req.session.color) {
+        const ColorHash = new ColorHash();
+        req.session.color = colorHash.hex(req.sessionID);
+        console.log(req.session.color, req.sessionID);
+    }
+    next();
+});
+
+app.use('/', indexRouter);
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+const server = app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
+
+webSocket(server, app, sessionMiddleware);
+
+
